e3561b197db09a3fee33c9f0b08165ec841add00
MattIPv4 <me@mattcowley.co.uk>
2022-05-04 21:50:04 +0100

    Add new embed for Glitch

---
+++ rules/embeds/glitch.js
@@ -0,0 +1,142 @@
+/*
+Copyright 2022 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+'use strict';
+
+/**
+ * @module rules/embeds/glitch
+ */
+
+/**
+ * Add support for [Glitch](https://glitch.com/) embeds in Markdown, as block syntax.
+ *
+ * The basic syntax is `[glitch <slug>]`. E.g. `[glitch hello-digitalocean]`.
+ * After the slug, some space-separated flags can be added (in any combination/order):
+ *
+ * - Add `noattr` to tell Glitch to not show the authors of the project.
+ * - Add `code` to set the Glitch embed to show the project code by default.
+ * - Add `notree` to set the Glitch embed to collapse the file tree by default.
+ * - Add any set of digits to set the height of the embed (in pixels).
+ *
+ * @example
+ * [glitch hello-digitalocean]
+ *
+ * [glitch hello-digitalocean code 512 notree]
+ *
+ * <div class="glitch-embed-wrap" style="height: 256px; width: 100%;">
+ *     <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?previewSize=100" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+ *         <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+ *     </iframe>
+ * </div>
+ *
+ * <div class="glitch-embed-wrap" style="height: 512px; width: 100%;">
+ *     <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?previewSize=0&sidebarCollapsed=true" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+ *         <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+ *     </iframe>
+ * </div>
+ *
+ * @type {import('markdown-it').PluginSimple}
+ */
+module.exports = md => {
+    /**
+     * Parsing rule for Glitch markup.
+     *
+     * @type {import('markdown-it/lib/parser_block').RuleBlock}
+     * @private
+     */
+    const glitchRule = (state, startLine, endLine, silent) => {
+        // If silent, don't replace
+        if (silent) return false;
+
+        // Get current string to consider (just current line)
+        const pos = state.bMarks[startLine] + state.tShift[startLine];
+        const max = state.eMarks[startLine];
+        const currentLine = state.src.substring(pos, max);
+
+        // Perform some non-regex checks for speed
+        if (currentLine.length < 10) return false; // [glitch a]
+        if (currentLine.slice(0, 8) !== '[glitch ') return false;
+        if (currentLine[currentLine.length - 1] !== ']') return false;
+
+        // Check for glitch match
+        const match = currentLine.match(/^\[glitch (\S+)((?: (?:noattr|code|notree|\d+))*)\]$/);
+        if (!match) return false;
+
+        // Get the slug
+        const slug = match[1];
+        if (!slug) return false;
+
+        // Get the raw flags
+        const flags = match[2];
+
+        // Get the height
+        const heightMatch = flags.match(/\d+/);
+        const height = heightMatch ? Number(heightMatch[0]) : 256;
+
+        // Defines if the embed should hide attribution
+        const noAttr = flags.includes('noattr');
+
+        // Defines if the embed should default to the code
+        const code = flags.includes('code');
+
+        // Defines if the embed should default to collpasing the file tree
+        const noTree = flags.includes('notree');
+
+        // TODO: Support path query param
+        // TODO: Support highlights query param (comma-separated list of line numbers)
+
+        // Update the pos for the parser
+        state.line = startLine + 1;
+
+        // Add token to state
+        const token = state.push('glitch', 'glitch', 0);
+        token.block = true;
+        token.markup = match[0];
+        token.glitch = { slug, height, noAttr, code, noTree };
+
+        // Done
+        return true;
+    };
+
+    md.block.ruler.before('paragraph', 'glitch', glitchRule);
+
+    /**
+     * Rendering rule for Glitch markup.
+     *
+     * @type {import('markdown-it/lib/renderer').RenderRule}
+     * @private
+     */
+    md.renderer.rules.glitch = (tokens, index) => {
+        const token = tokens[index];
+
+        // Escape some HTML
+        const slug = md.utils.escapeHtml(token.glitch.slug);
+        const slugUrl = encodeURIComponent(token.glitch.slug);
+
+        // Construct the params
+        const params = new URLSearchParams();
+        if (token.glitch.noAttr) params.append('attributionHidden', 'true');
+        params.append('previewSize', token.glitch.code ? '0' : '100');
+        if (token.glitch.noTree) params.append('sidebarCollapsed', 'true');
+
+        // Return the HTML
+        return `<div class="glitch-embed-wrap" style="height: ${token.glitch.height}px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/${slugUrl}?${params.toString()}" title="${slug} on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/${slugUrl}" target="_blank">View ${slug} on Glitch</a>
+    </iframe>
+</div>\n`;
+    };
+};
---
+++ rules/embeds/glitch.test.js
@@ -0,0 +1,78 @@
+/*
+Copyright 2022 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+'use strict';
+
+const md = require('markdown-it')().use(require('./glitch'));
+
+it('handles glitch embeds (not inline)', () => {
+    expect(md.render('[glitch hello-digitalocean]')).toBe(`<div class="glitch-embed-wrap" style="height: 256px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?previewSize=100" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+    </iframe>
+</div>
+`);
+});
+
+it('handles glitch embeds with no slug (no embed)', () => {
+    expect(md.render('[glitch ]')).toBe(`<p>[glitch ]</p>
+`);
+});
+
+it('handles glitch embeds with a custom height', () => {
+    expect(md.render('[glitch hello-digitalocean 512]')).toBe(`<div class="glitch-embed-wrap" style="height: 512px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?previewSize=100" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+    </iframe>
+</div>
+`);
+});
+
+it('handles glitch embeds with the noattr flag', () => {
+    expect(md.render('[glitch hello-digitalocean noattr]')).toBe(`<div class="glitch-embed-wrap" style="height: 256px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?attributionHidden=true&previewSize=100" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+    </iframe>
+</div>
+`);
+});
+
+it('handles glitch embeds with the code flag', () => {
+    expect(md.render('[glitch hello-digitalocean code]')).toBe(`<div class="glitch-embed-wrap" style="height: 256px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?previewSize=0" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+    </iframe>
+</div>
+`);
+});
+
+it('handles glitch embeds with the notree flag', () => {
+    expect(md.render('[glitch hello-digitalocean notree]')).toBe(`<div class="glitch-embed-wrap" style="height: 256px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?previewSize=100&sidebarCollapsed=true" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+    </iframe>
+</div>
+`);
+});
+
+it('handles glitch embeds with multiple flags combined (noattr, custom height, notree, code)', () => {
+    expect(md.render('[glitch hello-digitalocean noattr 512 notree code]')).toBe(`<div class="glitch-embed-wrap" style="height: 512px; width: 100%;">
+    <iframe src="https://glitch.com/embed/#!/embed/hello-digitalocean?attributionHidden=true&previewSize=0&sidebarCollapsed=true" title="hello-digitalocean on Glitch" allow="geolocation; microphone; camera; midi; encrypted-media; xr-spatial-tracking; fullscreen" allowFullScreen style="height: 100%; width: 100%; border: 0;">
+        <a href="https://glitch.com/edit/#!/hello-digitalocean" target="_blank">View hello-digitalocean on Glitch</a>
+    </iframe>
+</div>
+`);
+});
