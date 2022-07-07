ea532e566fa9619517234cf62aef5a021573cbbb
MattIPv4 <me@mattcowley.co.uk>
2022-07-07 21:47:53 +0100

    Add full tests for columns

--- rules/embeds/columns.test.js
+++ rules/embeds/columns.test.js
@@ -18,10 +18,162 @@ limitations under the License.
 
 const md = require('markdown-it')().use(require('./columns'));
 
-it('handles columns (not inline)', () => {
-    expect(md.render('[column\ncontent\n]\n[column\ncontent\n]\noutside')).toBe(``);
+it('handles two columns (not inline)', () => {
+    expect(md.render('[column\none\n]\n[column\ntwo\n]')).toBe(`<div class="columns">
+<div class="column">
+<p>one</p>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+</div>
+`);
 });
 
-it('handles columns with an extra linebreak between', () => {
-    expect(md.render('[column\ncontent\n]\n\n[column\ncontent\n]\noutside')).toBe(``);
+it('handles two columns with an extra linebreak between', () => {
+    expect(md.render('[column\none\n]\n\n[column\ntwo\n]')).toBe(`<div class="columns">
+<div class="column">
+<p>one</p>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+</div>
+`);
+});
+
+it('handles a single column (no embed)', () => {
+    expect(md.render('[column\none\n]')).toBe(`<p>[column
+one
+]</p>
+`);
+});
+
+it('handles two columns with one unclosed (no embed)', () => {
+    expect(md.render('[column\none\n]\n[column\ntwo')).toBe(`<p>[column
+one
+]
+[column
+two</p>
+`);
+});
+
+it('handles two columns with an extra multi-line bracket pair inside', () => {
+    expect(md.render('[column\none\n[\ntest\n]\n]\n\n[column\ntwo\n]')).toBe(`<div class="columns">
+<div class="column">
+<p>one
+[
+test
+]</p>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+</div>
+`);
+});
+
+it('handles two columns with an extra single-line bracket pair inside', () => {
+    expect(md.render('[column\none\n[test]\n]\n\n[column\ntwo\n]')).toBe(`<div class="columns">
+<div class="column">
+<p>one
+[test]</p>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+</div>
+`);
+});
+
+it('handles two columns with an unclosed bracket pair inside (no embed)', () => {
+    expect(md.render('[column\none\n[\ntest\n]\n[column\ntwo\n]')).toBe(`<p>[column
+one
+[
+test
+]
+[column
+two
+]</p>
+`);
+});
+
+it('handles nested columns', () => {
+    expect(md.render('[column\none\n\n[column\nnested one\n]\n[column\nnested two\n]\n]\n[column\ntwo\n]')).toBe(`<div class="columns">
+<div class="column">
+<p>one</p>
+<div class="columns">
+<div class="column">
+<p>nested one</p>
+</div>
+<div class="column">
+<p>nested two</p>
+</div>
+</div>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+</div>
+`);
+});
+
+it('handles more columns', () => {
+    expect(md.render('[column\none\n]\n[column\ntwo\n]\n[column\nthree\n]\n[column\nfour\n]')).toBe(`<div class="columns">
+<div class="column">
+<p>one</p>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+<div class="column">
+<p>three</p>
+</div>
+<div class="column">
+<p>four</p>
+</div>
+</div>
+`);
+});
+
+const mdOuterClass = require('markdown-it')().use(require('./columns'), { outerClassName: 'test' });
+
+it('handles two columns with a custom outer class', () => {
+    expect(mdOuterClass.render('[column\none\n]\n\n[column\ntwo\n]')).toBe(`<div class="test">
+<div class="column">
+<p>one</p>
+</div>
+<div class="column">
+<p>two</p>
+</div>
+</div>
+`);
+});
+
+const mdInnerClass = require('markdown-it')().use(require('./columns'), { innerClassName: 'test' });
+
+it('handles two columns with a custom inner class', () => {
+    expect(mdInnerClass.render('[column\none\n]\n\n[column\ntwo\n]')).toBe(`<div class="columns">
+<div class="test">
+<p>one</p>
+</div>
+<div class="test">
+<p>two</p>
+</div>
+</div>
+`);
+});
+
+const mdClass = require('markdown-it')().use(require('./columns'), { outerClassName: 'outer', innerClassName: 'inner' });
+
+it('handles two columns with custom inner and outer classes', () => {
+    expect(mdClass.render('[column\none\n]\n\n[column\ntwo\n]')).toBe(`<div class="outer">
+<div class="inner">
+<p>one</p>
+</div>
+<div class="inner">
+<p>two</p>
+</div>
+</div>
+`);
 });
